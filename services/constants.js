78c39f50ffd96ecf48c796e1337d6f0040dfa1fb
Heorhii Aksiuk <aksiuk.heorhii@gmail.com>
2022-04-05 13:06:55 +0300

    refactoring

--- services/constants.js
+++ services/constants.js
@@ -3,10 +3,13 @@ const PORT = 3000
 const httpCode = {
   OK: 200,
   CREATED: 201,
-  NO_CONTENT: 204,
   BAD_REQUEST: 400,
   NOT_FOUND: 404,
   INTERNAL_SERVER_ERROR: 500,
+}
+
+const regExp = {
+  PHONE: /^((8|\+7)[\- ]?)?(\(?\d{3}\)?[\- ]?)?[\d\- ]{7,10}$/,
 }
 
 const nameLength = {
@@ -14,13 +17,9 @@ const nameLength = {
   MAX: 30,
 }
 
-const regExp = {
-  PHONE: /^((8|\+7)[\- ]?)?(\(?\d{3}\)?[\- ]?)?[\d\- ]{7,10}$/,
-}
-
 module.exports = {
   PORT,
   httpCode,
-  nameLength,
   regExp,
+  nameLength,
 }
